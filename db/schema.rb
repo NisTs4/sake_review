554ef3c4f50bab238d38ad02d3ab76e8a7fba087
Nishibori Tetsuya <tetsuya.nishibori@gmail.com>
2015-06-16 22:13:07 +0900

    keep at 2015/06/16

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426111218) do
+ActiveRecord::Schema.define(version: 20150512152040) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150426111218) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
